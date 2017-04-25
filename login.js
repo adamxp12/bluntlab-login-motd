b3a571ec652b96679ce15961480d45d430a39cf8
Adam Blunt <adamxp12@ADAMXP12.server.adamxp12.com>
2017-04-25 13:22:13 +0100

    Add Better OS, Add mem/uptime

--- login.js
+++ login.js
@@ -1,4 +1,24 @@
 var colour = require('colour'),
-    os = require('os');
+    os = require('os'),
+    getos = require('getos'),
+    osName = require('os-name'),
+    pretty = require('prettysize'),
+    humanizeDuration = require('humanize-duration')
+
+//hostname
 console.log("    "+"Welcome to ".grey+os.hostname().rainbow.bold);
-console.log("    "+"OS:".grey+os.type().yellow.bold+" Ver: ".grey+os.release().yellow.bold)
+
+//os type and version (if linux then run getos instead for proper distro recognition)
+if(os.type() === "linux") {
+    getos(function(e,os) {
+        if(e) return console.log(e)
+        console.log("OS:".grey +os.os+" Distro: ".grey+os.dist+" Ver:".grey+os.release)
+    })
+} else {
+    console.log("    "+"OS:".grey+osName().yellow.bold+" Ver: ".grey+os.release().yellow.bold)
+}
+
+//mem
+ console.log("    "+"Memory: ".grey.bold+" Free: ".grey+pretty(os.freemem()).red.bold + " Used: ".grey + pretty(os.totalmem() - os.freemem()).green.bold + " Total: ".grey + pretty(os.totalmem()).blue.bold)
+//uptime
+ console.log("    "+"Uptime: ".grey+humanizeDuration(1000 * Math.round(os.uptime())).cyan.bold)
